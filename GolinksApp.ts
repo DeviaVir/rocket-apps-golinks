ee58545c2e13725601729fa41b348b9b40658a48
Chase Sillevis <chase@sillevis.net>
2022-12-20 19:01:21 +0100

    patches

--- GolinksApp.ts
+++ GolinksApp.ts
@@ -1,5 +1,7 @@
 import {
     IAppAccessors,
+    IConfigurationExtend,
+    IEnvironmentRead,
     ILogger,
 } from '@rocket.chat/apps-engine/definition/accessors';
 import { App } from '@rocket.chat/apps-engine/definition/App';
@@ -8,5 +10,10 @@ import { IAppInfo } from '@rocket.chat/apps-engine/definition/metadata';
 export class GolinksApp extends App {
     constructor(info: IAppInfo, logger: ILogger, accessors: IAppAccessors) {
         super(info, logger, accessors);
+    }
+
+    public async initialize(configurationExtend: IConfigurationExtend, environmentRead: IEnvironmentRead): Promise<void> {
+        await this.extendConfiguration(configurationExtend, environmentRead);
+        this.getLogger().log('GoLinks App Initialized');
     }
 }
