782d7e42071e4522e125acb1db31e3b891a39679
Chase Sillevis <chase@sillevis.net>
2022-12-22 11:49:43 +0100

    milestone: first working version

--- GolinksApp.ts
+++ GolinksApp.ts
@@ -3,17 +3,94 @@ import {
     IConfigurationExtend,
     IEnvironmentRead,
     ILogger,
+    IHttp,
+    IMessageBuilder,
+    IRead,
+    IPersistence,
 } from '@rocket.chat/apps-engine/definition/accessors';
 import { App } from '@rocket.chat/apps-engine/definition/App';
+import { IMessage, IPreMessageSentModify } from '@rocket.chat/apps-engine/definition/messages';
 import { IAppInfo } from '@rocket.chat/apps-engine/definition/metadata';
+import { SettingType } from '@rocket.chat/apps-engine/definition/settings';
 
-export class GolinksApp extends App {
+export class GolinksApp extends App implements IPreMessageSentModify {
+    private matcher: RegExp = /(go\/(\S+)?)/gi;
+
     constructor(info: IAppInfo, logger: ILogger, accessors: IAppAccessors) {
         super(info, logger, accessors);
+        this.getLogger();
     }
 
     public async initialize(configurationExtend: IConfigurationExtend, environmentRead: IEnvironmentRead): Promise<void> {
+        configurationExtend.settings.provideSetting({
+            id: 'url',
+            type: SettingType.STRING,
+            packageValue: '',
+            required: true,
+            public: false,
+            i18nLabel: 'Full URL of your shortlink service, e.g.: https://golinks.rocket.chat/',
+        });
+
         await this.extendConfiguration(configurationExtend, environmentRead);
         this.getLogger().log('GoLinks App Initialized');
     }
+
+    public async checkPreMessageSentModify(message: IMessage, read: IRead, http: IHttp): Promise<boolean> {
+        if (typeof message.text !== 'string') {
+            return false;
+        }
+
+        const result = message.text.match(this.matcher);
+
+        return result ? result.length !== 0 : false;
+    }
+
+    public async executePreMessageSentModify(
+        message: IMessage, builder: IMessageBuilder, read: IRead, http: IHttp, persistence: IPersistence): Promise<IMessage> {
+        const msg = builder.getMessage();
+        if (typeof msg.text !== 'string') {
+            return await builder.getMessage();
+        }
+
+        const golinks = msg.text.match(this.matcher);
+        if (golinks && golinks.length > 0) {
+            const url = await read.getEnvironmentReader().getSettings().getById('url');
+            if (url.value == null || url.value == '') {
+                return await builder.getMessage();
+            }
+
+            const usedLinks: Set<string> = new Set();
+            golinks.sort((a, b) => b.length - a.length);
+            const uniqueGoLinks = [...new Set(golinks)];
+            this.getLogger().log(uniqueGoLinks);
+            for (const link of uniqueGoLinks) {
+                const parts = link.split('/');
+                const endLink = parts.slice(1).join('/');
+
+                const newLink = `[${link}](${url.value}${endLink})`
+                this.getLogger().log(endLink);
+                msg.text = msg.text.replace(link, (match, link) => {
+                    if (usedLinks.has(link)) {
+                        return match;
+                    }
+
+                    if (typeof msg.text !== 'string') {
+                        return match;
+                    }
+
+                    if (msg.text[link - 1] === '[') {
+                        // Return the original link if it is preceded by a blockquote
+                        return match;
+                    }
+
+                    usedLinks.add(link);
+                    return newLink;
+                });
+            }
+            builder.setData(msg);
+        }
+
+        return await builder.getMessage();
+    }
+
 }
